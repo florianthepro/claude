7841379a6070d74584fe1bdbc8c2f32f0551ccac
florianthepro <florianthepro@icloud.com>
2026-09-16 17:27:26 +0200

    Delete domainfinder directory

--- domainfinder/__init__.py
+++
@@ -1,2 +0,0 @@
-"""domainfinder -- verifiziert registrierbare Domainnamen statt Vorschlaege."""
-__version__ = "1.0.0"
--- domainfinder/gen/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Kandidatenquellen A bis M. Bewusst getrennt gehalten."""
-from . import (brand5, branding, compound, itpair, itroot, jargon,  # noqa: F401
-               kurzmarke, latinform, lexicon, morpheme, phonotactic,
-               serioes, vollstaendig)
